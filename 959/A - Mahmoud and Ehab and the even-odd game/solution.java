2cfae422cb5c651b06a34e93625bd3a767532216
Piyush Tiwari <piyusht122005@gmail.com>
2026-02-28 11:23:36 +0530

    Add Mahmoud and Ehab and the even-odd game [A] from Codeforces

---
+++ 959/A - Mahmoud and Ehab and the even-odd game/solution.java
@@ -0,0 +1,1 @@
+import java.util.Scanner; public class Mahmoud_and_Ehab_and_the_even_odd_game {    public static void main(String[] args) {        Scanner sc = new Scanner(System.in);        int n = sc.nextInt();         if (n%2==0) System.out.println("Mahmoud");        else System.out.println("Ehab");    }}
