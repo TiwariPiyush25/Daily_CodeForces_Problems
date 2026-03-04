aa1586a39657887ae6041ba0e9abfc84eeac244e
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-04 15:41:09 +0530

    Add Ambitious Kid [A] from Codeforces

---
+++ 1866/A - Ambitious Kid/solution.java
@@ -0,0 +1,1 @@
+import java.util.Scanner; public class Ambitious_Kid {    public static void main(String[] args) {        Scanner sc = new Scanner(System.in);        int n = sc.nextInt();         int min = Integer.MAX_VALUE;         for (int i=0;i<n;i++){            int x = sc.nextInt();            min = Math.min(min,Math.abs(x));        }         System.out.println(min);    }}
